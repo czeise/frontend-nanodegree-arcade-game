f1a52b7bc0dded490b5267cd72caae22e305c569
Craig Zeise <craig.zeise@gmail.com>
2017-01-22 14:36:24 -0600

    Add gulp

---
+++ docs/js/app.js
@@ -0,0 +1,1 @@
+var Enemy=function(){this.sprite="images/enemy-bug.png"};Enemy.prototype.update=function(e){},Enemy.prototype.render=function(){ctx.drawImage(Resources.get(this.sprite),this.x,this.y)},document.addEventListener("keyup",function(e){var t={37:"left",38:"up",39:"right",40:"down"};player.handleInput(t[e.keyCode])});
